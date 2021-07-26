c7b0298ec85a27c7e69d8af3a1d12a6738a9da79
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-07-26 10:04:27 +0800

    data are coming from array

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,76 @@
 $(document).ready(function () {
+	// laod pets
+	let all_pets = [
+		{
+			id: 1,
+			pet_name: "Garfield",
+			pet_type: "Cat",
+		},
+		{
+			id: 2,
+			pet_name: "Doreamon",
+			pet_type: "Cat",
+		},
+		{
+			id: 3,
+			pet_name: "Snoopy",
+			pet_type: "Dog",
+		},
+		{
+			id: 4,
+			pet_name: "Daffy",
+			pet_type: "Duck",
+		},
+		{
+			id: 5,
+			pet_name: "Pen Pen",
+			pet_type: "Penguin",
+		},
+	];
+
+	function load_pets() {
+		let pets = ``;
+		for (let i = all_pets.length - 1; i >= 0; i--) {
+			pets += `<div class="pet-info" data-pet="${all_pets[i].pet_name}">`;
+			pets += `	<p>${all_pets[i].pet_name}</p>`;
+			pets += `	<p>${all_pets[i].pet_type}</p>`;
+			pets += `	<div class="action">`;
+			pets += `		<a href="#" role="button" class="detailsPet-open-modal" data-toggle="modal" data-target=".detailsPet"><i class="far fa-list-alt"></i> Details</a>`;
+			pets += `		<a href="#" role="button" data-toggle="modal" class="editPet-open-modal" data-target=".editPet"><i class="fas fa-pen-square"></i> Edit</a>`;
+			pets += `	</div>`;
+			pets += `</div>`;
+		}
+
+		$(".pet-lists").html(pets);
+	}
+
+	load_pets();
+
+	//////////////////////////////////////////////////////////////////////////
 	// add pet
 	$(".btn-add").click(function () {
 		if ($("#pet-name").val() == "") {
 			alert("pet name must have a value");
 			return false;
 		}
-		let new_pet = ``;
-		new_pet += `<div class="pet-info">`;
-		new_pet += `	<p>${$("#pet-name").val()}</p>`;
-		new_pet += `	<p>${$("#pet-type").val()}</p>`;
-		new_pet += `	<div class="action">`;
-		new_pet += `		<a href=""  role="button" class="detailsPet-open-modal" data-toggle="modal" data-target=".detailsPet"><i class="far fa-list-alt"></i> Details</a>`;
-		new_pet += `		<a ref="#" role="button" data-toggle="modal" class="editPet-open-modal" data-target=".editPet"><i class="fas fa-pen-square"></i> Edit</a>`;
-		new_pet += `	</div>`;
-		new_pet += `</div>`;
+
+		let new_pet = {
+			id: all_pets.length + 1,
+			pet_name: $("#pet-name").val(),
+			pet_type: $("#pet-type").val(),
+		};
+
+		all_pets.push(new_pet);
+
+		load_pets();
+
+		// $(".pet-lists").html(pets);
 
 		$(".added-pet-name").html($("#pet-name").val());
 
 		$("#pet-name").val("");
 		$("#pet-type").val($("#pet-type option:first").val());
 
-		$(".pet-lists").prepend(new_pet);
 		$(".toast").toast("show");
 	});
 
@@ -49,6 +99,14 @@ $(document).ready(function () {
 		// alert("edit");
 		// console.log($(".editPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html());
 		let pet_to_edit = $(".editPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html();
+
+		for (let i = 0; i < all_pets.length; i++) {
+			if (pet_to_edit == all_pets[i].pet_name) {
+				all_pets[i].pet_type = $(`#pet-type-edit`).val();
+			}
+		}
+
+		load_pets();
 
 		console.log($(".pet-lists").children().data());
 	});
